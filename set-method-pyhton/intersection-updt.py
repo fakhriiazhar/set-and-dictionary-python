71e70951ec576e5042e7e3bff04808acf42e9ca3
Fakhrii Azhar <154650463+fakhriiazhar@users.noreply.github.com>
2024-10-19 13:47:47 +0800

    Create intersection-updt.py

---
+++ set-method-pyhton/intersection-updt.py
@@ -0,0 +1,6 @@
+#intersection update method
+nama = {"fakhrii", "azhar", 2311102441169}
+nama2 = {"fakhrii", "azhar", "pahri", 2311102441169}
+print(nama)
+nama.intersection_update(nama2)
+print(nama)
